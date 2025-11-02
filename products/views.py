2a566d9acc1995e431897c6270204ed67d9e2e63
unknown <rouchashoori@gmail.com>
2025-11-02 22:07:44 +0330

    Add viewsets for category views

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from products.serializers import CategorySerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework import viewsets
 from products.models import Category
 from rest_framework.permissions import IsAuthenticated
 
 
-class CategoryViews(ViewSet):
+class CategoryViews(viewsets.ModelViewSet):
 
     serializer_class = CategorySerializer
     permission_classes = [
