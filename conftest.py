7b01b1761f2fcd5b8461fc68869ffb28bbbc5eb1
unknown <rouchashoori@gmail.com>
2025-11-03 20:05:16 +0330

    Add product_data fixture to receive data needed to create a product

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 import pytest
 from accounts.models import User, Role
+from products.models import Category
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.test import APIClient
 
-
+# returns authorized client to send HTTP request and a user object to be used accross user relations
 @pytest.fixture
 def make_authorized_client(db):
     def _make(phone_number):
@@ -18,7 +19,7 @@ def make_authorized_client(db):
 
     return _make
 
-
+# returns category data needed to create one
 @pytest.fixture
 def category_data():
     return {
@@ -27,3 +28,32 @@ def category_data():
         "description": "this is test",
         "display_order": 1,
     }
+
+# returns products data needed to create one
+@pytest.fixture
+def product_data(category_data):
+
+    """ Arguments:
+        is_post_method_data(Default = Fasle): if turns true in field that are foreign keys will be a number
+        passed using HTTP method and if it is false it will be and Instance of model  
+
+        
+       """
+
+    category = Category.objects.create(**category_data)
+
+    def _make(is_post_method_data = False):
+        
+        return {
+            "category_id": category.category_id if is_post_method_data else category,
+            "name": "perfume",
+            "brand": "Tom ford",
+            "slug": "tom-ford-perfume",
+            "sku": "test",
+            "price": 99.99,
+            "weight_kg": 1.25,
+            "dimensions": "2cm x 10cm",
+            "description": "this is test",
+        }
+    
+    return _make
