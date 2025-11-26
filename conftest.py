7b5d537473e20171b6b4fe65db2b63fa0c4cb8d9
Amirali Ashoori <102422438+itisroach@users.noreply.github.com>
2025-11-26 18:42:42 +0330

    Implement CRUD for Products and Categories
    * Add viewset, serializers and urls for category
    
    * Add test for category
    
    * Add create method for category serializer
    
    * Add viewsets for category views
    
    * Add pytest initialize file
    
    * Add fixtures for pytest
    
    * Fix product models
    
    * Remove unecessary import causing ci fails
    
    * Add product_data fixture to receive data needed to create a product
    
    * Fix models basemodel_ptr error due to using inheritence from basemodel class
    
    * Add tests, urls and views for products
    
    * Fix bugs in tests and fixtures
    
    * Fix icon_url bug
    
    * Fix confilict

---
+++ conftest.py
@@ -0,0 +1,60 @@
+import pytest
+from accounts.models import User, Role
+from products.models import Category
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.test import APIClient
+
+# returns authorized client to send HTTP request and a user object to be used accross user relations
+@pytest.fixture
+def make_authorized_client(db):
+    def _make(phone_number):
+        role = Role.objects.create(name="Test", permissions="{}")
+        user = User.objects.create_user(
+            phone_number=phone_number, role=role, password="something"
+        )
+        access_token = str(RefreshToken.for_user(user).access_token)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+        return client, user
+
+    return _make
+
+# returns category data needed to create one
+@pytest.fixture
+def category_data():
+    return {
+        "icon_url": "https://test.com",
+        "name": "sport",
+        "slug": "sport-thisistest",
+        "description": "this is test",
+        "display_order": 1,
+    }
+
+# returns products data needed to create one
+@pytest.fixture
+def product_data(category_data):
+
+    """ Arguments:
+        is_post_method_data(Default = Fasle): if turns true in field that are foreign keys will be a number
+        passed using HTTP method and if it is false it will be and Instance of model  
+
+        
+       """
+
+    category = Category.objects.create(**category_data)
+
+    def _make(is_post_method_data = False):
+        
+        return {
+            "category_id": category.category_id if is_post_method_data else category,
+            "name": "perfume",
+            "brand": "Tom ford",
+            "slug": "tom-ford-perfume",
+            "sku": "test",
+            "price": 99.99,
+            "weight_kg": 1.25,
+            "dimensions": "2cm x 10cm",
+            "description": "this is test",
+        }
+    
+    return _make
--- products/models.py
+++ products/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 from accounts.models import User
+    
 
 
-class BaseModels(models.Model):
+class Product(models.Model):
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class Product(BaseModels):
+    is_active = models.BooleanField(default=True)
 
     product_id = models.AutoField(primary_key=True)
     category_id = models.ForeignKey(
         "Category", null=True, blank=True, on_delete=models.SET_NULL
     )
 
-    brand = models.CharField(max_length=128)
+    brand = models.CharField(max_length=100)
+    slug = models.CharField(max_length=255, unique=True)
+    sku = models.CharField(max_length=100, unique=True)
 
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    weight_kg = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight_kg = models.DecimalField(max_digits=8, decimal_places=3)
+    dimensions = models.CharField(max_length=50)
+
+    view_count = models.PositiveIntegerField(default=0)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+
+    review_count = models.PositiveIntegerField(default=0)
+    sold_count = models.PositiveIntegerField(default=0)
+
+    is_featured = models.BooleanField(default=False)
 
 
 class ProductImage(models.Model):
@@ -28,13 +39,20 @@ class ProductImage(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     url = models.URLField()
-    alt_text = models.CharField(max_length=32)
-    position = models.IntegerField()
-
+    thumbnail_url = models.URLField()
+    alt_text = models.CharField(max_length=255)
+    position = models.IntegerField(default=0)
+    is_primary = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Category(BaseModels):
+class Category(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     category_id = models.AutoField(primary_key=True)
     parent_id = models.ForeignKey(
@@ -46,16 +64,50 @@ class Category(BaseModels):
     icon_url = models.URLField()
     banner_url = models.URLField(null=True, blank=True)
 
-    is_active = models.BooleanField(default=True)
     display_order = models.IntegerField(null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "Categories"
 
 
-class Discount(BaseModels):
-    # TODO: implement this
-    pass
+class Discount(models.Model):
+
+    TYPE_CHOICES = [
+        ("percentage", "درصد"),
+        ("fixed", "ثابت"),
+        ("free_shipping", "حمل و نقل رایگان"),
+    ]
+
+    APPLIES_TO_CHOICES = [
+        ("all", "همه"),
+        ("products", "محصولات"),
+        ("categories", "دسته بندی ها"),
+    ]
+
+    discount_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    code = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=20, default="percentage", choices=TYPE_CHOICES)
+
+    value = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    min_purchase = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    max_discount = models.DecimalField(max_digits=10, decimal_places=2)
+    usage_limit_total = models.PositiveIntegerField(null=True)
+    usage_limit_per_user = models.PositiveIntegerField(null=True)
+    used_count = models.PositiveIntegerField(default=0)
+
+    applies_to = models.CharField(max_length=20, default="all", choices=TYPE_CHOICES)
+    traget_ids = models.JSONField(default=list)
+    first_purchase_only = models.BooleanField(default=False)
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Cart(models.Model):
 
@@ -75,17 +127,17 @@ class Cart(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # the price of products in cart before applying discount
-    subtotal = models.DecimalField(max_digits=6, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     discount_id = models.ForeignKey(
         Discount, null=True, blank=True, on_delete=models.SET_NULL
     )
 
     # the amount of money that user should be paying
-    total_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=active)
-    items = models.JSONField()
+    items = models.JSONField(default=list)
 
     expires_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
