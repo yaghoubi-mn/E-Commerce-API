330f5e1ce6137a1f35dd9cc58d8a0fd3bdc1352c
Amirali Ashoori <102422438+itisroach@users.noreply.github.com>
2025-12-22 19:18:32 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
-from .views import CustomTokenObtainPairView, TempRegisterView, RoleViewset
+from .views import CustomTokenObtainPairView, RoleViewset
 from .views import (
     CustomTokenObtainPairView,
     RegisterView,
@@ -27,7 +27,6 @@ urlpatterns = [
     path("accounts/verify-otp/", VerifyOTPView.as_view(), name="verify_otp"),
     path('accounts/login/', CustomTokenObtainPairView.as_view(), name='login'),
     path('accounts/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/register/', TempRegisterView.as_view(), name='temp_register'),
     path('accounts/reset-password/', ResetPasswordView.as_view(), name='reset_password'),
     path('accounts/change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('accounts/profile/', ProfileView.as_view(), name='profile'),
