4e6e8c0b6f4853574c1184b79e7bc3dd1f251d3b
Mohammadamin Yaghoubi <yaghoubi.mn@gmail.com>
2025-12-24 19:31:06 +0330

    feat: implement product comments endpoints (closes #31)

--- products/permission.py
+++ products/permission.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAdminUser(BasePermission):
@@ -14,3 +14,10 @@ class IsAdminUser(BasePermission):
             return True
 
         return False
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
