d635cec112a895f2bc703ae86fcd4d29c419ac90
Mohammadamin Yaghoubi <145757780+yaghoubi-mn@users.noreply.github.com>
2025-12-27 23:58:11 +0330

    feat: add Comment, CommentVote, Notification models (#33)

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    link_url = models.CharField(max_length=500, null=True)
+    is_read = models.BooleanField(default=False)
+
+    PRIORITY_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    )
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES)
+    read_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
